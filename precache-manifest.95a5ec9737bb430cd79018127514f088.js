e05c4c6b66f29095c264957bef51900de53dc458
Nathan Griffiths <ngriffiths@postmedia.com>
2023-06-22 10:18:54 -0700

    Updates

---
+++ precache-manifest.95a5ec9737bb430cd79018127514f088.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df797e4a17d13915d7538bd5fe5bd9a0",
+    "url": "/govt-promise-tracker/index.html"
+  },
+  {
+    "revision": "29c75903a49f23d6a794",
+    "url": "/govt-promise-tracker/static/css/2.d5bec64a.chunk.css"
+  },
+  {
+    "revision": "24e3732071dffdd70966",
+    "url": "/govt-promise-tracker/static/css/main.ae83f8e0.chunk.css"
+  },
+  {
+    "revision": "29c75903a49f23d6a794",
+    "url": "/govt-promise-tracker/static/js/2.519c4ad5.chunk.js"
+  },
+  {
+    "revision": "24e3732071dffdd70966",
+    "url": "/govt-promise-tracker/static/js/main.470dbfce.chunk.js"
+  },
+  {
+    "revision": "2f176b7542f24a9eee48",
+    "url": "/govt-promise-tracker/static/js/runtime~main.7ef6218f.js"
+  },
+  {
+    "revision": "6f6753fd733a216b24786a168657540a",
+    "url": "/govt-promise-tracker/static/media/BentonSansCond-Bold.6f6753fd.otf"
+  },
+  {
+    "revision": "a6640d8951e3d6c523b2b58b0db9e351",
+    "url": "/govt-promise-tracker/static/media/BentonSansCond-BoldItalic.a6640d89.otf"
+  },
+  {
+    "revision": "69a4217c7da7462a2511b1a8ecaefecf",
+    "url": "/govt-promise-tracker/static/media/BentonSansCond-RegItalic.69a4217c.otf"
+  },
+  {
+    "revision": "43cd4ac75abf53e257d21366d3322b0e",
+    "url": "/govt-promise-tracker/static/media/BentonSansCond-Regular.43cd4ac7.otf"
+  },
+  {
+    "revision": "cdd38a860699d029f95e76f21e51e216",
+    "url": "/govt-promise-tracker/static/media/Shift-Bold.cdd38a86.otf"
+  },
+  {
+    "revision": "5c3edbfdeedc59bb35fac3bf1a461926",
+    "url": "/govt-promise-tracker/static/media/Shift-BoldItalic.5c3edbfd.otf"
+  }
+]);
